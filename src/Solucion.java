3bb91538c0fcc46c60e789195e1750f7f514b2ab
Arturo802 <eteandres01@gmail.com>
2025-03-18 01:04:07 +0100

    primer commit

---
+++ src/Solucion.java
@@ -0,0 +1,14 @@
+import java.lang.StringBuilder;
+class Solution{
+
+
+    /**
+     * @param "String"
+     * @return cadenas de tipo camelCase
+     * @example"the-stealth-warrior"se convierte en"theStealthWarrior"
+     *"The_Stealth_Warrior"se convierte en"TheStealthWarrior"
+     *"The_Stealth-Warrior"se convierte en"TheStealthWarrior"*/
+   public  static String toCamelCase(String s){
+        return "";
+    }
+}
